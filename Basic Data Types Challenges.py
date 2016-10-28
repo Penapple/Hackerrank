c29c751d5cd4e3b96fb951fc95a58e9d61c27921
Keyan(Kyle) Yang <kxy156@case.edu>
2016-10-28 17:34:47 -0400

    Update Basic Data Types Challenges.py

--- Basic Data Types Challenges.py
+++ Basic Data Types Challenges.py
@@ -3,3 +3,14 @@ n = int(input())
 num = map(int, input().split())
 print (sorted(list(set(num)))[-2])          #set function turns unique number
         
+
+# Nested Lists        
+n = int(input())
+l = []
+for _ in range(n):
+    name = input()
+    grade = float(input())
+    l.append([name, grade])     #generate list
+
+secondhi = sorted(list(set([marks for name, marks in l])))[1]       #list comprehension -- only select marks for marks and names
+print('\n'.join([a for a, b in sorted(l) if b == secondhi]))
