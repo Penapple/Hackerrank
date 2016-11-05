72de39041264143ca018174438464dd010e05a74
Keyan(Kyle) Yang <kxy156@case.edu>
2016-11-05 17:43:11 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,3 +1,3 @@
 #Upper to lower, lower to upper case
 st = input()
-print(''.join(i.upper() if i.islower() else i.lower() for i in st)) #'i for i in st' is objective. Both join([]) and join(i for i in x) work
+print(''.join(i.upper() if i.islower() else i.lower() for i in st)) #'i for i in st' is object. Both join([]) and join(i for i in x) work
