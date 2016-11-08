3c014bd0fcc123691b8f6eac28de814149a053c2
Keyan(Kyle) Yang <kxy156@case.edu>
2016-11-07 23:04:36 -0500

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,4 +1,8 @@
-#Arrays
 import numpy as np
+#Arrays
 a = np.array(list(input().split()), float)
 print(a[::-1])
+
+#Shape and Reshape
+a = np.array(list(map(int, input().split())))
+print(np.reshape(a,(3,3)))
