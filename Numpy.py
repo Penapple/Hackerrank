52b910dcec25d2de275318ffb368036b619203ac
Keyan(Kyle) Yang <kxy156@case.edu>
2016-11-07 23:22:53 -0500

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -6,3 +6,9 @@ print(a[::-1])
 #Shape and Reshape
 a = np.array(list(map(int, input().split())))
 print(np.reshape(a,(3,3)))
+
+#Transpose and Flatten
+n, m = map(int, input().split())
+a = np.array(list(input().split() for _ in range(n)), int)
+print(a.transpose())
+print(a.flatten())
