7ae58c381a5c7b385cd5b9b8c04a389351db4b84
Keyan(Kyle) Yang <kxy156@case.edu>
2016-11-11 17:10:14 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -5,5 +5,7 @@ print(''.join(i.upper() if i.islower() else i.lower() for i in st)) #'i for i in
 #Strings Validators
 S = input()
 print([s for s in S if s.isupper()])        #character in list; for before if
-
 print(any(s.isupper() for s in S))      #Amazing built-in function any
+
+#Text alignment
+print(input().ljust(25, '-')    #or use rjust, center
