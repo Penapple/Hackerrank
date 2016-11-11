870e10aad84a9855c0fbdbe3c51990af90ee76a8
Keyan(Kyle) Yang <kxy156@case.edu>
2016-11-11 16:43:43 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,3 +1,9 @@
 #Upper to lower, lower to upper case
 st = input()
 print(''.join(i.upper() if i.islower() else i.lower() for i in st)) #'i for i in st' is object. Both join([]) and join(i for i in x) work
+
+#Strings Validators
+S = input()
+print([s for s in S if s.isupper()])        #character in list
+
+print(any(s.isupper() for s in S))      #Amazing built-in function any
